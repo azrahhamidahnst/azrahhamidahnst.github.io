743985fe3ed986ceae431b2d701a4fb6681176b9
azrahhamidahnst <azrahhamidahnst@gmail.com>
2026-04-15 22:23:10 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("sqlXXX.epizy.com", "if0_41670617", "azrah2626", "if0_41670617_PerpusSDSinarMulia");
+$conn = mysqli_connect("azrah.infinityfreeapp.com", "if0_41670617", "azrah2626", "if0_41670617_PerpusSDSinarMulia");
 
 if (!$conn) {
     die("Koneksi gagal");
